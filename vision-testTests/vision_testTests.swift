915766744ee491d383fa1e8171ba841042e700de
mrevanzak <mrevanzak@gmail.com>
2025-06-04 09:39:54 +0700

    Initial Commit

---
+++ vision-testTests/vision_testTests.swift
@@ -0,0 +1,16 @@
+//
+//  vision_testTests.swift
+//  vision-testTests
+//
+//  Created by Revanza Kurniawan on 04/06/25.
+//
+
+import Testing
+
+struct vision_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
